991b643ac788c7bb0a870d490dec0f15875685a9
nick.818@frtk.ru <nick.818@frtk.ru@0879719b-bd20-6c4d-6a25-18cb37d9fc63>
2011-05-02 12:26:07 +0000

    New message from commit

--- tests/test2.s
+++ tests/test2.s
@@ -1,12 +1,13 @@
-#.section ".text"
+.section ".text"
 main:
-set %r11, 1	#con
-set %r10, 2	#con
-cmp %r11, %r10	#LEI2
+set %r9,1	#con
+set %r8,2	#con
+cmp %r9,%r8	#LEI2
 bler .L2	#LEI2
-mult %r1, %r11, %r10	#MULI2
-brr .L1	#JUMPV
+mult %r1,%r9,%r8	#MULI2
+set %r13,.L1	#ADDRGP2
+br %r13
 .L2:
-add %r1, %r11, %r10	#ADDI2
+add %r1,%r9,%r8	#ADDI2
 .L1:
 return
--- tests/test4.s
+++ tests/test4.s
@@ -1,22 +1,25 @@
-#.section ".text"
+.section ".text"
 main:
-set %r10, 1	#con
-set %r11, 2	#con
-cmp %r10, %r0	#EQI2
-bz .L4	#EQI2
-set %r13, 1	#con
-cmp %r10, %r13	#EQI2
-bz .L5	#EQI2
-br .L2	#JUMPV
+set %r8,1	#con
+set %r9,2	#con
+cmp %r8,%r0	#EQI2
+bzr .L4	#EQI2
+set %r13,1	#con
+cmp %r8,%r13	#EQI2
+bzr .L5	#EQI2
+set %r13,.L2	#ADDRGP2
+br %r13
 .L4:
-set %r11, 5	#con
-br .L3	#JUMPV
+set %r9,5	#con
+set %r13,.L3	#ADDRGP2
+br %r13
 .L5:
-set %r11, 10	#con
-br .L3	#JUMPV
+set %r9,10	#con
+set %r13,.L3	#ADDRGP2
+br %r13
 .L2:
-set %r11, 15	#con
+set %r9,15	#con
 .L3:
-add %r1, %r11, 0	#LOADI2
+add %r1, %r9, 0	#LOADI2
 .L1:
 return
