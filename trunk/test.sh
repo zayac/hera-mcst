1835dcc1f208dc3aec419ffee551aaa3723f2e0e
nick.818@frtk.ru <nick.818@frtk.ru@0879719b-bd20-6c4d-6a25-18cb37d9fc63>
2011-05-02 10:53:38 +0000

    New message from commit

--- trunk/test.sh
+++ trunk/test.sh
@@ -25,16 +25,19 @@ svn commit -m "New message from commit"
 
 for f in ./source/*.c
 do
-	echo -------------------$f
+	echo --------------------------------------Compile $f--------------------------------------
 	nameAsm=$(echo $f|sed 's/\(.*\)\.c/\1.s/')
 	nameObj=$(echo $f|sed 's/\(.*\)\.c/\1.o/')
 	#echo -------------------$nameAsm
 	#echo -------------------$nameObj
 	./compiler -S $f -o $nameAsm
 	cd ./source
+	echo --------------------------------------Assemble $f--------------------------------------
 	../asm ../$nameAsm
 	cd ../
-	#./sim $nameObj
+	echo --------------------------------------Simulate $f--------------------------------------
+	./sim $nameObj
+	
 done
  
 
