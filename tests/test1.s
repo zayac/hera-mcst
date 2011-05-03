83a2daaac35f356a59c8a27c6d8faf9c9df05d73
sergey.zhelnin@gmail.com <sergey.zhelnin@gmail.com@0879719b-bd20-6c4d-6a25-18cb37d9fc63>
2011-05-03 13:19:25 +0000

    Add the verilog of HERA files from 2010 team

--- tests/test1.s
+++
@@ -1,7 +0,0 @@
-.section ".text"
-main:
-set %r9,1	#con
-set %r8,2	#con
-add %r1,%r9,%r8	#ADDI2
-.L1:
-return
--- tests/test2.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-	int a = 1;
-	int b = 2;
-	if (a > b)
-		return a * b;
-	else
-		return a + b;
-}
